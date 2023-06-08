dd9c88c1feef4781a6d9c65540cc437aff841196
essoungou.wonga <essoungou.wonga@lao-sarl.cm>
2023-06-08 14:00:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const questionContainer = document.querySelector('.click-event');
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector('p');
+
+questionContainer.addEventListener("click", () => {
+    questionContainer.classList.toggle('question-clicked');
+})
+
+btn1.addEventListener("click", () => {
+    response.classList.add("show-response");
+    response.style.backgroundColor = "green";
+})
+btn2.addEventListener("click", () => {
+    response.classList.add("show-response");
+    response.style.backgroundColor = "red";
+})
